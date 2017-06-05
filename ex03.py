dde563b5400d5f67f06d276591499b724515ed76
SeeBenProgram <Ben@SeeBenProgram.com>
2017-06-05 19:35:31 -0400

    Update to Python 3.5 Syntax

--- ex03.py
+++ ex03.py
@@ -1,23 +1,23 @@
-print "I will now count my chickens:" # simple print statement
+print ("I will now count my chickens:") # simple print statement
 
-print "Hens", 25 + 30 / 6 # 30 / 6 = 5, plus 25 = 30 (order of operations applies)
-print "Roosters", 100 - 25 * 3 % 4 # 25 x 3 is 75, 75 divided by 4 yields a remainder of 3, 100 - 3 = 97
+print ("Hens", 25 + 30 / 6) # 30 / 6 = 5, plus 25 = 30 (order of operations applies)
+print ("Roosters", 100 - 25 * 3 % 4) # 25 x 3 is 75, 75 divided by 4 yields a remainder of 3, 100 - 3 = 97
 
-print "now I will count the eggs:"
+print ("now I will count the eggs:")
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.00 + 6 # 4 / 2 yields remainder of 0, 1 / 4 is .25; 6 - 5 + 0 - .25 + 6 = 6.75
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.00 + 6) # 4 / 2 yields remainder of 0, 1 / 4 is .25; 6 - 5 + 0 - .25 + 6 = 6.75
 
-print "Is it true that 3 + 2 < 5 - 7?"
+print ("Is it true that 3 + 2 < 5 - 7?")
 
-print 3 + 2 < 5 - 7 # inequality yields false because 5 is actually greater than -2
+print (3 + 2 < 5 - 7) # inequality yields false because 5 is actually greater than -2
 
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+print ("What is 3 + 2?", 3 + 2)
+print ("What is 5 - 7?", 5 - 7)
 
-print "Oh, that's why it's False."
+print ("Oh, that's why it's False.")
 
-print "How about some more."
+print ("How about some more.")
 
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+print ("Is it greater?", 5 > -2)
+print ("Is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= -2)
