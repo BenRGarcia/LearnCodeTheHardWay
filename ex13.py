6e995c845275a902d845e7171aff27bb9b33c088
Ben <ben@seebenprogram.com>
2017-06-09 12:41:39 -0400

    Complete ex13.py in Python 3.5 syntax

--- ex13.py
+++ ex13.py
@@ -2,7 +2,10 @@ from sys import argv
 
 script, first, second, third = argv
 
+argv = input("Type in the name of a vegetable: ")
+
 print ("The script is called:", script)
 print ("The first variable is:", first)
 print ("The second variable is:", second)
 print ("The third variable is:", third)
+print ("The user input variable (name of a vegetable) is:", argv)
