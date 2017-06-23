0e986fc455f5226ea177b716e1aa85ee35f82faa
Ben <ben@seebenprogram.com>
2017-06-23 13:32:05 -0400

    Begin ex22.py to review all lesson material

---
+++ ex22.py
@@ -0,0 +1,53 @@
+"""
+# What do you know so far?
+
+# ex00.py introduced:
+Opened the terminal, created a directory
+# ex01.py introduced:
+How to create "print" statements
+print
+# ex02.py introduced:
+How to create comments with """ """ and #
+# ex03.py introduced:
+How to do basic math functions within print statements
+Saw how some mathematical statements can result in either True or False
+# ex04.py introduced:
+declaring variables as numbers/floats, and using math w/ variables declared
+printing variable values within a print statement
+# ex05.py introduced:
+
+# ex06.py introduced:
+
+# ex07.py introduced:
+
+# ex08.py introduced:
+
+# ex09.py introduced:
+
+# e010.py introduced:
+
+# ex11.py introduced:
+
+# ex12.py introduced:
+
+# ex13.py introduced:
+
+# ex14.py introduced:
+
+# ex15.py introduced:
+
+# ex16.py introduced:
+
+# ex17.py introduced:
+
+# ex18.py introduced:
+
+# ex19.py introduced:
+
+# ex20.py introduced:
+
+# ex21.py introduced:
+
+
+
+"""
