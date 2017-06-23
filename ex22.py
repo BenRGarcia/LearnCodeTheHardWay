64578c3767069d857486854147e86c6afc514520
Ben <ben@seebenprogram.com>
2017-06-23 14:39:23 -0400

    Continue ex22.py progress

--- ex22.py
+++ ex22.py
@@ -15,27 +15,27 @@ Saw how some mathematical statements can result in either True or False
 declaring variables as numbers/floats, and using math w/ variables declared
 printing variable values within a print statement
 # ex05.py introduced:
-
+Using the % symbol to insert declared variables in a specific format into a print statement
 # ex06.py introduced:
-
+More examples of the %. Also concatenation of 2 variables using the + symbol
 # ex07.py introduced:
-
+concatenation of multiple string variables
 # ex08.py introduced:
-
+feeding % into a variable declared with more %'s
 # ex09.py introduced:
-
+print statement using quotes for a multi-line print statement
 # e010.py introduced:
-
+using \n for a new line, \t for a tab spacing in a print statement, \\ to escape a \
 # ex11.py introduced:
-
+getting user input from input()
 # ex12.py introduced:
-
+more advanced user input()
 # ex13.py introduced:
-
+the argv class/object, user input into the script via command line
 # ex14.py introduced:
-
+using the argv s'more, adding a formatting twist with the >
 # ex15.py introduced:
-
+using open(), .read, .close
 # ex16.py introduced:
 
 # ex17.py introduced:
